dba2ad4ebd91d656b672da3e094d6604f260d0fe
alisariaslan <acsayear@gmail.com>
2023-04-16 06:00:33 +0300

    pages

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-node main.js localhost 80 mongodb://admin:admin@127.0.0.1:27017/pakacraft -https openssl/key.pem openssl/cert.pem
-pause
+node main.js localhost 80 mongodb://127.0.0.1:27017/battleground -https openssl/key.pem openssl/cert.pem
+::pause
